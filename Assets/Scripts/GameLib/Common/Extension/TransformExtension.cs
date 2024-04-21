d3874cfe06d192db503b2c9e583a852bd46306f6
kirakuiin <wang.zhuowei@foxmail.com>
2024-04-21 14:17:32 +0800

    feat(common): ✨增加删除子节点的快捷操作。

---
+++ Assets/Scripts/GameLib/Common/Extension/TransformExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace GameLib.Common.Extension
+{
+    public static class TransformExtension
+    {
+        /// <summary>
+        /// 对所有的子节点执行同一操作。
+        /// </summary>
+        /// <param name="trans"></param>
+        /// <param name="func"></param>
+        public static void DoSomethingToAllChildren(this Transform trans, Action<GameObject> func)
+        {
+            var allChildren = from idx in Enumerable.Range(0, trans.childCount)
+                select trans.GetChild(idx).gameObject;
+            foreach (var gameObject in allChildren)
+            {
+                func(gameObject);
+            }
+        }
+
+        /// <summary>
+        /// 销毁全部子节点。
+        /// </summary>
+        /// <param name="trans"></param>
+        public static void DestroyAllChildren(this Transform trans)
+        {
+            trans.DoSomethingToAllChildren(Object.Destroy);
+        }
+    }
+}
